8a155f77a2260e64839811f35d85a642020726b0
nikhil bhaskar <perfectnikhil1697@gmail.com>
2017-11-04 23:54:01 +0530

    layout improved and functions added to js

--- basic_javascript/scripting.js
+++ basic_javascript/scripting.js
@@ -1,15 +1,44 @@
 function Insert()
 {
-  a=document.getElementsByTagName("input")
-  s_name=a[0].value
-  s_rollno=a[1].value
-  s_section=a[2].value
-  x=document.createElement("p")
-  spaces= "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" 
-  x.innerHTML=s_name  +spaces+
-             +s_rollno +spaces // &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp s_rollno  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-             + s_section
-  x.align="center"            
-  y=document.body
-  y.append(x)            
+  fetch=document.getElementsByTagName("input")
+  s_name=fetch[0].value
+  s_rollno=fetch[1].value
+  s_section=fetch[2].value
+  trow=document.createElement("tr")
+  butn=document.createElement("input")
+  butn.setAttribute('type','checkbox')
+  td_1=document.createElement("td")
+  td_2=document.createElement("td")
+  td_3=document.createElement("td")
+  trow.appendChild(butn)
+  trow.appendChild(td_1)
+  trow.appendChild(td_2)
+  trow.appendChild(td_3)
+  //spaces= "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" 
+  td_1.innerHTML= s_name
+  td_2.innerHTML=s_rollno
+  td_3.innerHTML=s_section
+  trow.align="center"
+  butn.align="center"
+  td_1.align="center" 
+  td_2.align="center"
+  td_3.align="center"
+             
+  tabl=document.getElementById('tab')
+  tabl.appendChild(trow)            
+}
+
+function Delete()
+{
+  $("#tab input:checkbox:checked").closest('tr').remove();
+
+}
+
+function Edit()
+{
+  fetch=document.getElementsByTagName("input")
+  s_name=fetch[0].value
+  s_rollno=fetch[1].value
+  s_section=fetch[2].value
+  $("#tab input:checkbox:checked").replaceWith(s_name);
 }
